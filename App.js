34ff5fbaffd9b50786418ad90f2ba75a4e2c5e70
Sewwandi weerasingha <sewwandi@gmail.com>
2024-03-17 12:28:13 +0530

    neww

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!!!</Text>
+      <Text>Hello sewwwwwfjjjf!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
