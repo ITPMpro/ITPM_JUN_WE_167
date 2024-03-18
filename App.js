f34e5f21116f56530bb12283e73fcf44be062ed5
IT21183218 <99754850+IT21183218@users.noreply.github.com>
2024-03-19 03:52:59 +0530

    SignInPush

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//@Format
+//@flow
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import SignInScreen from './src/screens/SignInScreen';
+//import CustomInput from './src/components/CustomInput';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>login</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App=()=>{
+    return(
+        <SafeAreaView style={styles.root}>
+           <SignInScreen/>
+           
+        </SafeAreaView>
+    );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+};
+const styles= StyleSheet.create({
+    root:{
+        flex:1,
+        backgroundColor:'#F9FBFC'
+    },
 });
+export default App;
