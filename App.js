00f5a582b0b632af1fd74649e4da6fa9b37aa6cb
Hansana <it21183218@my.sliit.lk>
2024-05-10 11:24:49 +0530

    update all files

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
-//@Format
-//@flow
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import SignInScreen from './src/screens/SignInScreen';
-//import CustomInput from './src/components/CustomInput';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screen/Login";
+import Register from "./screen/Register";
+import ViewProfile from "./screen/ViewProfile";
+import Home from "./screen/Home";
+import EditProfile from "./screen/EditProfile ";
+import SpinAndWin from "./screen/SpinAndWin";
 
-const App=()=>{
-    return(
-        <SafeAreaView style={styles.root}>
-           <SignInScreen/>
-           
-        </SafeAreaView>
-    );
+const Stack = createStackNavigator();
 
-};
-const styles= StyleSheet.create({
-    root:{
-        flex:1,
-        backgroundColor:'#F9FBFC'
-    },
-});
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="ViewProfile" component={ViewProfile} />
+        <Stack.Screen name="Home" component={Home} />
+
+        <Stack.Screen name="Spinner" component={SpinAndWin} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
